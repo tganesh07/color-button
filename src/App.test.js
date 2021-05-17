8d25b396f26b868ef46f12332ce77d17cbefb032
Thulasi <tganesh07@gmail.com>
2021-05-17 12:54:50 -0400

    new test case

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('click', () => {
+  render(
+    <div>
+      <label htmlFor='checkbox'>Check</label>
+      <input id='checkbox' type='checkbox' />
+    </div>
+  );
+
+  // TypeError: (0 , _dom.getConfig) is not a function
+  userEvent.click(screen.getByText('Check'));
+  expect(screen.getByLabelText('Check')).toBeChecked();
 });
